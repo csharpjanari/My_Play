28588cf1895553d83335074ffd9c8f91d8b9ce0b
Raul Janari <rdjanari1707@gmail.com>
2022-12-13 20:45:38 +0100

    Adding BsonSerializer for easier reading db

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,7 +1,10 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers(options =>
 {
     options.SuppressAsyncSuffixInActionNames = false;
@@ -9,6 +12,8 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 var app = builder.Build();
 
