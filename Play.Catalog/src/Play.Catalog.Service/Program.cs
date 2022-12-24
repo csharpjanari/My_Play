521bff038d8156f2771fb73c94642ae09358d558
Raul Janari <rdjanari1707@gmail.com>
2022-12-24 11:16:26 +0100

    Refactored MassTrans conf into the reusable NuGet

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,13 +1,10 @@
 using MassTransit;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Settings;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
 using Play.Common.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
-
-ServiceSettings serviceSettings = new();
-serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
 // Add services to the container.
 builder.Services.AddControllers(options =>
@@ -17,27 +14,13 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMongo()
-                .AddMongoRepository<Item>("items");
-
-builder.Services.AddMassTransit(x => 
-{
-    x.UsingRabbitMq((context, configurator) =>
-    {
-        var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-        configurator.Host(rabbitMQSettings.Host);
-        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-    });
-});
+                .AddMongoRepository<Item>("items")
+                .AddMassTransitWithRabbitMQ();
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();/*(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Catalog");
-    options.RoutePrefix = string.Empty;
-});
-*/
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
